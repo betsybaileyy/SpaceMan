ef85b0fc337f681fc7849f5c769a7236ef7b5f16
betsybaileyy <betsy.bailey80@gmail.com>
2018-11-28 15:45:22 -0800

    deciding how to write guess function

--- main.py
+++ main.py
@@ -3,5 +3,19 @@ import random
 letters_guessed = []
 correct_guesses = []
 
-name_list = ["Betsy", "Bailey", "Margaux", "Katie", "Joey", "Peyton", "Will", "Bess"]
+name_list = ["Betsy", "Bailey", "Margaux", "Caesar", "Katie", "Joey", "Peyton", "Will", "Bess", "Elliot", "Anna", "Chris"]
 secret_name = random.choice(name_list)
+
+guesses = 10
+word_space= "-" * len(secret_name)
+
+while guesses > 0
+    print("guess a letter!")
+    guess = input("I choose: ")
+
+    if guess in secret_name:
+
+
+
+def letter_input():
+    letter_guess = imp
