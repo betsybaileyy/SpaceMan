a0ec91feda7edb9c23eae81d0e24ca4f863c90b5
betsybaileyy <betsy.bailey80@gmail.com>
2018-11-27 23:24:54 -0800

    initalized a list of names to choose at random

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+
+letters_guessed = []
+correct_guesses = []
+
+name_list = ["Betsy", "Bailey", "Margaux", "Katie", "Joey", "Peyton", "Will", "Bess"]
+secret_name = random.choice(name_list)
